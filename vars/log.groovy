8d9c547abfc96586d2cb78d2024949dc2f629d16
saikumarram123 <61085482+saikumarram123@users.noreply.github.com>
2023-05-17 17:47:41 +0530

    Update log.groovy

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,7 +1,26 @@
-def info(message) {
-    echo "INFO: ${message}"
-}
-
-def warning(message) {
-    echo "WARNING: ${message}"
+// vars/evenOrOdd.groovy
+def call(int buildNumber) {
+  if (buildNumber % 2 == 0) {
+    pipeline {
+      agent any
+      stages {
+        stage('Even Stage') {
+          steps {
+            echo "The build number is even"
+          }
+        }
+      }
+    }
+  } else {
+    pipeline {
+      agent any
+      stages {
+        stage('Odd Stage') {
+          steps {
+            echo "The build number is odd"
+          }
+        }
+      }
+    }
+  }
 }
